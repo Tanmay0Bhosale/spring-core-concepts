396b0b17839d98260b565052536b56f79137bbc3
Tanmay <tanmaytanvi1234@gmail.com>
2026-03-10 13:52:35 +0530

    [Tanmay] : Added UC2 SLF4J Logger implementation

--- src/main/java/com/example/spring_core_concepts/SpringCoreConceptsApplication.java
+++ src/main/java/com/example/spring_core_concepts/SpringCoreConceptsApplication.java
@@ -1,14 +1,20 @@
 package com.example.spring_core_concepts;
 
-import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringCoreConceptsApplication {
 
+	private static final Logger logger =
+			LoggerFactory.getLogger(SpringCoreConceptsApplication.class);
+
 	public static void main(String[] args) {
 
-		SpringApplication.run(SpringCoreConceptsApplication.class, args);
+		logger.info("Spring Boot Application Started");
+		logger.debug("Debug log example");
+		logger.warn("Warning log example");
 
 	}
 }
