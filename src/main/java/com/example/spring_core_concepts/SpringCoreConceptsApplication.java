53f49052ca9ed4e3a310c2459001cbe398467d62
Tanmay <tanmaytanvi1234@gmail.com>
2026-03-10 13:00:48 +0530

    [Tanmay] : Refactored the code

--- src/main/java/com/example/spring_core_concepts/SpringCoreConceptsApplication.java
+++ src/main/java/com/example/spring_core_concepts/SpringCoreConceptsApplication.java
@@ -1,4 +1,4 @@
-package com.demo.springconcepts;
+package com.example.spring_core_concepts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
